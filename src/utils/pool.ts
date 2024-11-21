7e90c680a0c88e11ee7b2d1ebbccdbf43fac3aea
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-11-21 09:22:46 +0700

    fiX : adding signleton

--- src/utils/pool.ts
+++ src/utils/pool.ts
@@ -1,14 +1,33 @@
-import {Pool} from 'pg'
+import { Pool } from 'pg'
 import dotenv from 'dotenv'
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 dotenv.config()
 
 
 const projectUrl = process.env.PROJECT_URL as string;
 const projectApiKey = process.env.PROJECT_API_KEYS as string;
 
-export const supabase = createClient(projectUrl, projectApiKey);
 
+class SupabaseSingleton {
+    private static instance: SupabaseClient | null = null;
+
+    private constructor() {
+        // Prevent direct instantiation
+    }
+
+    public static getInstance(): SupabaseClient {
+        if (SupabaseSingleton.instance === null) {
+            console.log('Creating a new Supabase instance...');
+            SupabaseSingleton.instance = createClient(projectUrl, projectApiKey);
+        } else {
+            console.log('Supabase instance already exists.');
+        }
+        
+        return SupabaseSingleton.instance;
+    }
+}
+
+export const supabase = SupabaseSingleton.getInstance();
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
