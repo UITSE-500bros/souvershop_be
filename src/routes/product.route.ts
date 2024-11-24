a996f02334444b81556d5e8006254e5b10143876
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-11-24 23:03:31 +0700

    Update swagger

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,11 +4,15 @@ import {productController} from '../controllers';
 const router = Router();
 
 router.get('/', productController.getAllProducts);
-router.get('/inventories', productController.getAllInventories);
-router.get('/inventories/:product_id', productController.getInventoryByProductId);
+router.get('/inventory', productController.getAllInventories);
+router.get('/inventory/:product_id', productController.getInventoryByProductId);
 router.get('/:product_id', productController.getProductById);
 router.post('/', productController.createProduct);
 router.put('/:product_id', productController.updateProduct);
 router.delete('/:product_id', productController.deleteProduct);
+
+// Thiếu get product by category_id
+// Update getAllInventories cần name, main_img
+// Change /inventories/:product_id return id, name, main_img, price, quantity
 
 export default router;
