bed83120b52cfebecf33e88b9428bdc7094b1d1d
DPhngNam <22520908@gm.uit.edu.vn>
2024-12-12 23:42:37 +0700

    Update mail.service.ts

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -11,15 +11,21 @@ let transporter = nodemailer.createTransport({
     }
 });
 class MailService {
-
     async sendVerifiedEmail(email: string, token: string, template: string) {
         await transporter.sendMail({
             from: process.env.PROJECT_MAIL,
             to: email,
             subject: 'Verify your email',
             html: template
-            
+        });
+    }
 
+    async sendResetPasswordEmail(email: string, token: string, template: string) {
+        await transporter.sendMail({
+            from: process.env.PROJECT_MAIL,
+            to: email,
+            subject: 'Reset your password',
+            html: template
         });
     }
 }
