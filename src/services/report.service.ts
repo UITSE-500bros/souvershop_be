bd081f03bced0ef1b89f98fb556df746d33a0257
Nguyễn Chí Lâm <37863077+Lama2323@users.noreply.github.com>
2024-12-28 16:51:16 +0700

    change handle null from 0 to "0"

--- src/services/report.service.ts
+++ src/services/report.service.ts
@@ -4,19 +4,19 @@ class ReportService {
   async getReport() {
     try {
       const grnTotalResult = await pool.query('SELECT SUM(total) as grn_total FROM grn');
-      const buy_total = grnTotalResult.rows[0].grn_total || 0;
+      const buy_total = grnTotalResult.rows[0].grn_total || "0";
 
       const receiptTotalResult = await pool.query('SELECT SUM(total) as receipt_total FROM receipt');
-      const sale_total = receiptTotalResult.rows[0].receipt_total || 0;
+      const sale_total = receiptTotalResult.rows[0].receipt_total || "0";
 
       const categoryCountResult = await pool.query('SELECT COUNT(*) as category_count FROM category');
-      const category_count = categoryCountResult.rows[0].category_count || 0;
+      const category_count = categoryCountResult.rows[0].category_count || "0";
 
       const totalQuantityResult = await pool.query(`
         SELECT SUM((item ->> 'quantity')::numeric) as total_quantity
         FROM grn, json_array_elements(product_list) as item
       `);
-      const total_quantity = totalQuantityResult.rows[0].total_quantity || 0;
+      const total_quantity = totalQuantityResult.rows[0].total_quantity || "0";
 
       return {
         buy_total,
@@ -38,14 +38,14 @@ class ReportService {
 
       const grnTotalQuery = `SELECT SUM(total) as grn_total FROM grn WHERE created_at BETWEEN $1 AND $2`;
       const grnTotalResult = await pool.query(grnTotalQuery, [formattedBeginDate, formattedEndDate]);
-      const buy_total = grnTotalResult.rows[0].grn_total || 0;
+      const buy_total = grnTotalResult.rows[0].grn_total || "0";
 
       const receiptTotalQuery = `SELECT SUM(total) as receipt_total FROM receipt WHERE created_at BETWEEN $1 AND $2`;
       const receiptTotalResult = await pool.query(receiptTotalQuery, [formattedBeginDate, formattedEndDate]);
-      const sale_total = receiptTotalResult.rows[0].receipt_total || 0;
+      const sale_total = receiptTotalResult.rows[0].receipt_total || "0";
 
       const categoryCountResult = await pool.query('SELECT COUNT(*) as category_count FROM category');
-      const category_count = categoryCountResult.rows[0].category_count || 0;
+      const category_count = categoryCountResult.rows[0].category_count || "0";
 
       const totalQuantityQuery = `
         SELECT SUM((item ->> 'quantity')::numeric) as total_quantity
@@ -53,7 +53,7 @@ class ReportService {
         WHERE grn.created_at BETWEEN $1 AND $2
       `;
       const totalQuantityResult = await pool.query(totalQuantityQuery, [formattedBeginDate, formattedEndDate]);
-      const total_quantity = totalQuantityResult.rows[0].total_quantity || 0;
+      const total_quantity = totalQuantityResult.rows[0].total_quantity || "0";
 
       return {
         buy_total,
