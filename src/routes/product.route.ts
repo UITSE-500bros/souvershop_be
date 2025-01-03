34a14856949e926af8f76802b0500c978b0240b1
DPhngNam <22520908@gm.uit.edu.vn>
2025-01-03 10:39:07 +0700

    Update product.route.ts

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -6,11 +6,11 @@ const router = Router();
 router.get('/search_random', productController.getRandomProduct);
 router.get('/inventory', productController.getAllInventories);
 router.get('/inventory/:product_id', productController.getInventoryByProductId);
-router.get('/:product_id',authMiddleware, productController.getProductById);
-router.get('/',authMiddleware, productController.getAllProducts);
 router.post('/', productController.createProduct);
 router.put('/:product_id', productController.updateProduct);
 router.delete('/:product_id', productController.deleteProduct);
 router.get('/search/:product_name', productController.searchProductByName);
+router.get('/:product_id',authMiddleware, productController.getProductById);
+router.get('/',authMiddleware, productController.getAllProducts);
 
 export default router;
