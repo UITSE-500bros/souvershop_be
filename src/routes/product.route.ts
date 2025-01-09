d26e373e1f8364c406d48be62689ac002372ace1
YA DAT <22520243@gm.uit.edu.vn>
2025-01-09 11:30:10 +0700

    Update product.route.ts

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -11,9 +11,10 @@ router.post('/', productController.createProduct);
 router.put('/:product_id', productController.updateProduct);
 router.delete('/:product_id', productController.deleteProduct);
 router.get('/search/:product_name', productController.searchProductByName);
+router.put('/discountevent', authMiddleware, ownerController.createDiscount);
 router.get('/lookup', productController.getProductforLookup);
 router.get('/:product_id',authMiddleware, productController.getProductById);
 router.get('/',authMiddleware, productController.getAllProducts);
-router.put('/discountevent', authMiddleware, ownerController.createDiscount);
+
 
 export default router;
